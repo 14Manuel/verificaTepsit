22b08c215ae012835d9d2d471d37cae3c5269ea3
14Manuel <47051902+14Manuel@users.noreply.github.com>
2019-05-04 10:02:01 +0200

    Add files via upload

---
+++ uml/News.java
@@ -0,0 +1,17 @@
+package verifica;
+
+public class News 
+{
+	private static int id;
+	private String descrizione,titolo;
+	private int cmax;
+	private Commento[] commenti;
+	
+	public News(String descrizione,String titolo) {
+		this.descrizione = descrizione;
+		this.commenti = commenti;
+		this.cmax=100;
+	}
+	
+	
+}
